91152a5536a35787c78dd3683153eb7e953ac184
Mathias Perslev <mp@cerebriu.com>
2024-09-18 00:23:37 +0200

    Added example-downstream-analysis.py script

---
+++ example-downstream-analysis.py
@@ -0,0 +1,125 @@
+"""
+A minimal example of an analysis script that interfaces with the youSleep Portal.
+"""
+
+# Import inbuilt packages
+import logging
+import json
+from typing import List
+from argparse import ArgumentParser
+
+# Import third-party packages
+import mne
+
+# Define module logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    """
+    Parse the mandatory arguments & single custom --staging-window-length argument.
+    """
+    parser = ArgumentParser(
+        description="A minimal example of an analysis script that interfaces with the "
+        "youSleep Portal."
+    )
+
+    # The following arguments are mandatory and will always be passed to the analysis by the portal.
+    # They must be handled by the analysis script even if not used.
+    # See analysis config parameters.defaults field for a list of all required parameters.
+    parser.add_argument("--input-file", type=str, help="Path to the input EDF file.")
+    parser.add_argument(
+        "--events-file", type=str, help="Path to the input events file."
+    )
+    parser.add_argument("--output-file", type=str, help="Path to the output JSON file.")
+    parser.add_argument(
+        "--channel-names", type=str, nargs="+", help="List of channel names."
+    )
+    parser.add_argument(
+        "--channel-types",
+        type=str,
+        nargs="+",
+        help="List of channel types (e.g., 'EEG', 'EOG').",
+    )
+    parser.add_argument(
+        "--channel-units",
+        type=str,
+        nargs="+",
+        help="List of channel units (e.g., 'uV').",
+    )
+    parser.add_argument(
+        "--cpus", type=int, help="Number of CPUs available for parallel processing."
+    )
+    parser.add_argument("--memory-mib", type=int, help="Memory available in MiB.")
+
+    return parser.parse_args()
+
+
+def create_event(label: str, start_ms: int, end_ms: int, channels: List[str]) -> dict:
+    """
+    Returns an event dictionary with the given label, start and end times.
+    """
+    return {
+        "start_time_ms": start_ms,  # Start time of the event in milliseconds
+        "end_time_ms": end_ms,  # End time of the event in milliseconds
+        "label": label,  # Valid EDF+ label (e.g., 'Sleep stage ?')
+        "channels": channels,  # Optional channel names the event refers to. Empty == all channels.
+        # "probability": None,     # If a probabilistic event, a float in [0, 1] representing the probability.
+        # "value": None,           # If a valued event, a float representing the value.
+    }
+
+
+def main():
+    """
+    The main analysis function which does roughly the following:
+    1. Parse the arguments.
+    2. Load the EDF file and validates its length matches the events file.
+    3. Writes 'EDF Arousal' events whereevere the events file has 'Sleep stage ?' events.
+    """
+    # Parse the arguments
+    args = parse_args()
+    logger.info("Running analysis with the following arguments: %s", vars(args))
+
+    # Read the EDF file
+    logger.info("Reading EDF file...")
+    edf_file = mne.io.read_raw_edf(args.input_file, preload=False)
+    sampling_rate = edf_file.info["sfreq"]
+
+    # Compute how many epochs of 'staging_window_length' (seconds)
+    n_samples = edf_file.n_times
+    length_ms = int(n_samples / sampling_rate * 1000)
+
+    # Load the events
+    logger.info("Loading events...")
+    with open(args.events_file, "r", encoding="utf-8") as f:
+        events = json.load(f)
+
+    # Check that the last event ends at the end of the recording
+    last_event = events[-1]
+    if last_event["end_time_ms"] != length_ms:
+        raise ValueError(
+            f"Last event ends at {last_event['end_time_ms']} ms, but the recording ends "
+            f"at {length_ms} ms."
+        )
+
+    # Create 'EDF Arousal' events matching the 'Sleep stage ?' events
+    logger.info("Creating 'EDF Arousal' events...")
+    arousal_events = []
+    for event in events:
+        arousal_event = create_event(
+            "EDF Arousal",
+            event["start_time_ms"],
+            event["end_time_ms"],
+            args.channel_names,
+        )
+        arousal_events.append(arousal_event)
+
+    # Write events to output file
+    logger.info("Saving %d events to %s", len(arousal_events), args.output_file)
+    with open(args.output_file, "w", encoding="utf-8") as f:
+        json.dump(arousal_events, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
